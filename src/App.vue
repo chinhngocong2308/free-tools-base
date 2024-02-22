f09519a36cb18ebf6d9e7b659e08ddbb4313fa2d
chinhnc <chinhnc@tigren.com>
2024-02-22 23:31:05 +0700

    styling new layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <br/>
+    <!-- <br/>
     <a href="/tinh-thue-tncn">Tinh Thue TNCN</a> |
-    <a href="/tinh-bhxh-bat-buoc">Tinh BHXH</a>
+    <a href="/tinh-bhxh-bat-buoc">Tinh BHXH</a> -->
   <router-view/>
 
 </template>
@@ -25,11 +25,10 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', 'Segoe UI', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
